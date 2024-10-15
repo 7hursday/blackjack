529762ca32b347b03e1f05d9f34afcd4b92049aa
nerea <mhernere@gmail.com>
2024-10-15 16:21:39 +0200

    Update bjServerFunciones.c

---
+++ bjServerFunciones.c
@@ -0,0 +1,15 @@
+#include "funciones.h"
+
+// Acciones de la baraja
+
+void cargarBaraja(Carta baraja[])
+{
+    
+}
+
+void barajarCartas(Carta baraja[])
+{
+
+}
+
+int calcularPuntos(Carta mano[], int numeroCartas);
