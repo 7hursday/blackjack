12396efca5761d18b1cfa61651b12fb2ee68b1b0
nerea <mhernere@gmail.com>
2024-10-15 17:11:07 +0200

    Update bjServerFunciones.h

--- bjServerFunciones.h
+++ bjServerFunciones.h
@@ -1,18 +1,20 @@
 #ifndef BJSERVERFUNCIONES_H
 #define BJSERVERFUNCIONES_H
 
+#define MSG_SIZE 250
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 // Estructuras necesarias para el juego
 
-typedef struct {
+/*typedef struct {
     char palo[10]; // tipo de carta en una cadena de caracteres
     int valor; // número de la carta
 } Carta;
 
-/* Estructura alternativa
+// Estructura alternativa
 typedef enum {Picas, Diamantes, Corazones, Treboles} Palo;
 
 typedef struct {
@@ -22,14 +24,14 @@ typedef struct {
 */
 
 typedef struct {
-    Carta cartas[11];
+    int carta[11];
     int manoCartas; // cantidad de cartas por mano
     int puntuacion;
 } ManoJugador;
 
 typedef struct {
-    int id;
-    char nombre[50];
+    char nombre[MSG_SIZE];
+    char contrasena[MSG_SIZE];
     ManoJugador mano;
     int sesionIniciada;
     int estaConectado;
@@ -38,18 +40,14 @@ typedef struct {
 typedef struct {
     Jugador jugador1;
     Jugador jugador2;
-    Carta baraja[52]; // todas las cartas de la baraja
+    int baraja[52]; // todas las cartas de la baraja
     int cartaActual;
 } Partida;
 
-// Funciones de usuario 
-// int registrarUsuario(const char* usuario, const char* contrasena);
-int comprobarUsuario();
-int verificarInicioSesion();
-
 // Acciones de la baraja
-void cargarBaraja(Carta baraja[]);
-void barajarCartas(Carta baraja[]);
-int calcularPuntos(Carta mano[], int numeroCartas);
+
+void cargarBaraja(int baraja[]);
+void barajarCartas(int baraja[]);
+void emparejarValCartas();
 
 #endif
