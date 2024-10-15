40f3cbc60b1c7c32b58d7b094b00b808b13f04f6
nerea <mhernere@gmail.com>
2024-10-15 17:11:51 +0200

    Update bjServerFunciones.c

--- bjServerFunciones.c
+++ bjServerFunciones.c
@@ -2,14 +2,24 @@
 
 // Acciones de la baraja
 
-void cargarBaraja(Carta baraja[])
+void cargarBaraja(int baraja[]) // genera las 52 cartas del juego
 {
-    
+    for(int i; i < 52; i++)
+    {
+        baraja[i] = i;
+    }
 }
 
-void barajarCartas(Carta baraja[])
+void barajarCartas(int baraja[]) // randomiza las cartas
 {
+    srand(time(NULL));
 
+    for(int i = 51; i < 0; i--)
+    {
+        int j = rand() % (i + 1);
+        int aux = baraja[i];
+        baraja[i] = baraja[j];
+        baraja[j] = aux;
+    }
 }
 
-int calcularPuntos(Carta mano[], int numeroCartas);
