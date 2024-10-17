02b13cb97f320d205544bab9c551cbe2e1ecbc83
nerea <mhernere@gmail.com>
2024-10-17 18:33:22 +0200

    Update bjServerFunciones.h
    Se han definido las funciones de la baraja.

--- bjServerFunciones.h
+++ bjServerFunciones.h
@@ -1,47 +1,49 @@
 #ifndef BJSERVERFUNCIONES_H
 #define BJSERVERFUNCIONES_H
 
-#define MSG_SIZE 250
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <string.h>
 
-// Estructuras necesarias para el juego
+#define MSG_SIZE 250
+#define MAX_CARTAS_MANO 11
+#define MAX_CARTAS_BARAJA 52
 
+// 1.   ESTRUCTURAS DE JUEGO
 typedef struct {
-    int carta[11];
-    int manoCartas; // cantidad de cartas por mano
+    int carta[MAX_CARTAS_MANO];     // cartas que posee el jugador (max 11)
+    int cartasPorMano;              // cuantas cartas tiene carta
     int puntuacion;
 } ManoJugador;
 
 typedef struct {
-    char nombre[MSG_SIZE];
-    char contrasena[MSG_SIZE];
-    ManoJugador mano;
-    int sesionIniciada;
-    int estaConectado;
+    char nombre[MSG_SIZE];          // usuario
+    char contrasena[MSG_SIZE];      // contraseña
+    ManoJugador mano;               // cartas del jugador y los puntos
+    int sesionIniciada;             // ha iniciado sesion
+    int estaConectado;              // esta conectado
 } Jugador;
 
 typedef struct {
-    Jugador jugador1;
-    Jugador jugador2;
-    int baraja[52]; // todas las cartas de la baraja
-    int cartaActual;
+    int baraja[4][13];              // 4 palos y 13 cartas (2 a 10, J, Q, K, A)
+    int cartasPorPalo[4];           // Cantidad de cartas por palo
+    int cartaActual;                // Siguiente carta a sacar
+    Jugador jugador1;               // jugador 1            
+    Jugador jugador2;               // jugador 2            
 } Partida;
 
-// Acciones de la baraja
-
-/*El algoritmo funciona de tal manera que se asocian 13 cartas
-a cada uno de los correspondientes palos*/
-
-void cargarBaraja(int baraja[]);
-void barajarCartas(int baraja[]);
-const char *obtenerPalo(int carta);
-int obtenerValor(int carta);
-void repartirCarta(int baraja[], int *cartaActual, ManoJugador *mano);
-void imprimirMano(ManoJugador *mano);
-
+// 2.   FUNCIONES DE GESTIÓN DE LA BARAJA
+void inicializarBaraja(Partida *partida);
+void barajarBaraja(Partida *partida);
+int sacarCarta(Partida *partida);
+void reiniciarBaraja(Partida *partida);
+int todasCartasJugadas(Partida *partida);
 int calcularPuntuacion(ManoJugador *mano);
 
+// 3.   FUNCIONES DE GESTIÓN DEL JUGADOR
+void agregarCartaMano(ManoJugador *mano, int carta);
+void resetearMano(ManoJugador *mano);
+
+// 4.   FUNCIONES DE GESTIÓN DE LA PARTIDA
 #endif
