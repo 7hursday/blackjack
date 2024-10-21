7eb92f3dfb70f0c83f50e34b090c7cbe16942fe8
nerea <mhernere@gmail.com>
2024-10-21 08:58:57 +0200

    Update cliente.c

--- cliente.c
+++ cliente.c
@@ -1,4 +1,4 @@
-	#include <stdio.h>
+#include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -10,48 +10,41 @@
 #include <arpa/inet.h>
 
 
-int main ( )
-{
-  
+int main(){
 	/*---------------------------------------------------- 
 		Descriptor del socket y buffer de datos                
 	-----------------------------------------------------*/
-	int sd;
-	struct sockaddr_in sockname;
+	int sd_cliente;
+	struct sockaddr_in cliente;
 	char buffer[250];
-	socklen_t len_sockname;
-    	fd_set readfds, auxfds;
-    	int salida;
-    	int fin = 0;
-	
+	socklen_t len_cliente;
+    fd_set readfds, auxfds;
+    int salida;
+    int fin = 0;
     
 	/* --------------------------------------------------
 		Se abre el socket 
 	---------------------------------------------------*/
-  	sd = socket (AF_INET, SOCK_STREAM, 0);
-	if (sd == -1)
-	{
+  	sd_cliente = socket (AF_INET, SOCK_STREAM, 0);
+	if (sd_cliente == -1){
 		perror("No se puede abrir el socket cliente\n");
-    		exit (1);	
+    	exit (1);	
 	}
 
-   
-    
 	/* ------------------------------------------------------------------
 		Se rellenan los campos de la estructura con la IP del 
 		servidor y el puerto del servicio que solicitamos
 	-------------------------------------------------------------------*/
-	sockname.sin_family = AF_INET;
-	sockname.sin_port = htons(2000);
-	sockname.sin_addr.s_addr =  inet_addr("127.0.0.1");
+	cliente.sin_family = AF_INET;
+	cliente.sin_port = htons(2000);
+	cliente.sin_addr.s_addr =  inet_addr("127.0.0.1");
 
 	/* ------------------------------------------------------------------
 		Se solicita la conexión con el servidor
 	-------------------------------------------------------------------*/
-	len_sockname = sizeof(sockname);
+	len_cliente = sizeof(cliente);
 	
-	if (connect(sd, (struct sockaddr *)&sockname, len_sockname) == -1)
-	{
+	if (connect(sd_cliente, (struct sockaddr *)&cliente, len_cliente) == -1){
 		perror ("Error de conexión");
 		exit(1);
 	}
@@ -60,69 +53,56 @@ int main ( )
     FD_ZERO(&auxfds);
     FD_ZERO(&readfds);
     
-    FD_SET(0,&readfds);
-    FD_SET(sd,&readfds);
-    printf("OPCIONES DEL SERVIDOR;\n");
+    FD_SET(0, &readfds); // 0 --> sd_cliente
+    FD_SET(sd_cliente, &readfds);
 
-    printf("USUARIO usuario\n");
-    printf("PASSWORD contraseña\n");
-    printf("REGISTRO –u usuario –p password\n");
-    printf("INICIAR-PARTIDA\n");
-    printf("PEDIR-CARTA\n");
-    printf("PLANTARME\n");
-    printf("SALIR\n");
+    printf("Opciones del servidor:\n");
+    printf("\tUSUARIO usuario\n");
+    printf("\tPASSWORD contraseña\n");
+    printf("\tREGISTRO -u usuario -p password\n");
+    printf("\tINICIAR-PARTIDA\n");
+    printf("\tPEDIR-CARTA\n");
+    printf("\tPLANTARME\n");
+    printf("\tSALIR\n");
     
 	/* ------------------------------------------------------------------
 		Se transmite la información
 	-------------------------------------------------------------------*/
-	do
-	{
+	do{
         auxfds = readfds;
-        salida = select(sd+1,&auxfds,NULL,NULL,NULL);
+        salida = select(sd_cliente+1,&auxfds,NULL,NULL,NULL);
         
         //Tengo mensaje desde el servidor
-        if(FD_ISSET(sd, &auxfds)){
+        if(FD_ISSET(sd_cliente, &auxfds)){
+            bzero(buffer, sizeof(buffer));
+            recv(sd_cliente, buffer, sizeof(buffer), 0);
             
-            bzero(buffer,sizeof(buffer));
-            recv(sd,buffer,sizeof(buffer),0);
+            printf("\n%s\n", buffer);
             
-            printf("\n%s\n",buffer);
-            
-            if(strcmp(buffer,"Demasiados clientes conectados\n") == 0)
+            if(strcmp(buffer, "Demasiados clientes conectados\n") == 0)
                 fin =1;
             
-            if(strcmp(buffer,"Desconexión servidor\n") == 0)
+            if(strcmp(buffer, "Desconexión servidor\n") == 0)
                 fin =1;
             
         }
-        else
-        {
-            
+        else{
             //He introducido información por teclado
             if(FD_ISSET(0,&auxfds)){
-                bzero(buffer,sizeof(buffer));
+                bzero(buffer, sizeof(buffer));
                 
-                fgets(buffer,sizeof(buffer),stdin);
+                fgets(buffer, sizeof(buffer), stdin);
                 
-                if(strstr(buffer,"REGISTRAR") == NULL){
-                        fin = 1;
-						
-                
+                if(strcmp(buffer,"SALIR\n") == 0){
+                    fin = 1;
                 }
                 
-                send(sd,buffer,sizeof(buffer),0);
-                
+                send(sd_cliente, buffer, sizeof(buffer), 0);
             }
-            
-            
-        }
-        
-        
-				
+        }		
     }while(fin == 0);
 		
-    close(sd);
+    close(sd_cliente);
 
     return 0;
-		
 }
